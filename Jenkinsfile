99404f8cbf76b78fedcb50d52b16696f76a25f05
lazlab9 <azlab9_cbe@learnlytica.onmicrosoft.com>
2026-01-14 15:36:48 +0530

    Second commit

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,7 +12,7 @@ pipeline {
     stage('Checkout Code') {
       steps {
         git branch: 'main', 
-            url: 'https://github.com/ak2104/Fundamentals-of-Terraform-with-Jenkins-Terraform-CI-CD-and-Remote-Backend-Azure-.git'
+            url: 'https://github.com/inamhaque2002-cloud/terraform-project.git'
       }
     }
 
--- main.tf
+++ main.tf
@@ -23,12 +23,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "AamirJenkinsTerraformRG2"
+  name     = "InamJenkinsTerraformRG2"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "AamirJenkinsVNet2"
+  name                = "InamJenkinsVNet2"
   address_space       = ["10.10.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
